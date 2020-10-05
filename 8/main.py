95f11fe2f5b057461e24f89edc7a47bb782f0b83
dom38 <dominicbyrne38@hotmail.com>
2020-10-05 21:11:58 +0100

    Add exercises

---
+++ 8/main.py
@@ -0,0 +1,32 @@
+# Print out the third letter in this variable:
+
+tester = "PrintTheThirdLetter"
+print() # <--- Put your answer in this print statement
+
+# Sort this list alphabetically and then print it:
+
+sortMe = ["Suna", "Freya", "Yuki", "Ichiro", "Ys", "Dogfart"]
+print()
+
+# Create a new file called big_dickheads.txt
+
+
+
+# Write the above list to big_dickheads.txt
+
+
+
+# Add the date to big_dickheads.txt WITHOUT overwriting the list already in there
+
+
+
+# Create a function that adds together 1 to X, X being the argument for the function
+# For example, function_name(5) would return "15", because 1+2+3+4+5 = 15
+# function_name(3) would return 6, because 1+2+3 = 6
+
+
+
+# Create a function that takes 2 int arguments and returns 'true' if the first 
+# number can be divided by the second number, and false otherwise. 
+# eg. funcction_name(12, 6) returns true. funcction_name(12, 7) returns false. 
+# As a tip, google 'python modulo operator'
